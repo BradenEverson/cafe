28534cfaba151a52fc9336225c178ab618ee9964
BradenEverson <bradeneverson@gmail.com>
2024-09-01 09:37:46 -0500

    Add new methods for asynchronously updating the list of orders

--- src/coffee_state.rs
+++ src/coffee_state.rs
@@ -16,8 +16,37 @@ pub struct State {
     orders: Vec<Order>,
 }
 
+impl State {
+    pub fn all_orders(&self) -> &[Order] {
+        &self.orders
+    }
+
+    pub fn add_order(&mut self, new_order: Order) {
+        self.orders.push(new_order)
+    }
+
+    pub fn delete_order(&mut self, index: usize) -> bool {
+        if index <= self.orders.len() {
+            self.orders.remove(index);
+            true
+        } else {
+            false
+        }
+    }
+}
+
 pub struct CoffeeService {
     state: Arc<RwLock<State>>,
+}
+
+impl CoffeeService {
+    pub async fn add_order(&mut self, new_order: Order) {
+        self.state.write().await.add_order(new_order)
+    }
+
+    pub async fn delete_order(&mut self, index: usize) -> bool {
+        self.state.write().await.delete_order(index)
+    }
 }
 
 impl CoffeeService {
@@ -33,7 +62,7 @@ impl Service<Request<body::Incoming>> for CoffeeService {
 
     fn call(&self, req: Request<body::Incoming>) -> Self::Future {
         let response = Response::builder().status(StatusCode::OK);
-        let state = self.state.clone();
+        let state = self.clone();
 
         let res = match req.method() {
             &Method::GET => {
@@ -52,6 +81,9 @@ impl Service<Request<body::Incoming>> for CoffeeService {
                 "/new_coffee" => {
                     todo!("Match query parameters for size, special mode and time");
                     response.body(Full::new(Bytes::copy_from_slice(b"Added!")))
+                }
+                "/delete" => {
+                    todo!("Match query parameters to get index of deleted request, delete it");
                 }
                 _ => response.body(Full::new(Bytes::copy_from_slice(&[]))),
             },
